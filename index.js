b36f5a2948fec051c10d42c926ba8e4e915ac065
AntoninaVi <tonatonotonya@gmail.com>
2022-11-09 00:48:12 +0700

    counters

--- index.js
+++ index.js
@@ -6,5 +6,23 @@
 // for (i = 0; i < fields.length; i++) {
 //     if (fields[i].value) {
 //         console.log('field is blank', fields[i])
-//     }
-// }
+
+
+let minusBtn = document.getElementById("minusbtn");
+let count = document.getElementById("count");
+let plusBtn = document.getElementById("plusbtn");
+
+let countNum = 0;
+count.innerHTML = countNum;
+
+minusBtn.addEventListener("click", () => {
+	countNum -= 1;
+	count.innerHTML = countNum;
+});
+
+plusBtn.addEventListener("click", () => {
+	countNum += 1;
+	count.innerHTML = countNum;
+});
+
+
