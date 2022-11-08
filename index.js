321a9b1a52d3ef20c828ef9039a44f44463615e4
AntoninaVi <tonatonotonya@gmail.com>
2022-11-08 21:54:51 +0700

    Input 4

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const fields = document.querySelector("form__input")
+// const fields = document.querySelector("form__input")
 
-document.querySelector(".form__input").addEventListener("keyup", function(){
-    this.value = this.value.replace(Value);
-});
-for (i = 0; i < fields.length; i++) {
-    if (fields[i].value) {
-      console.log('field is blank', fields[i])
-    }
-  }
+// document.querySelector(".form__input").addEventListener("keyup", function () {
+//     this.value = this.value.replace(Value);
+// });
+// for (i = 0; i < fields.length; i++) {
+//     if (fields[i].value) {
+//         console.log('field is blank', fields[i])
+//     }
+// }
