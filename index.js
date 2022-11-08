fcb80cb757f6e9d8e7c3ebb1e7e933ffe8094947
AntoninaVi <tonatonotonya@gmail.com>
2022-11-09 00:56:16 +0700

    script?

--- index.js
+++ index.js
@@ -8,21 +8,37 @@
 //         console.log('field is blank', fields[i])
 
 
-let minusBtn = document.getElementById("minusbtn");
-let count = document.getElementById("count");
-let plusBtn = document.getElementById("plusbtn");
+// let minusBtn = document.getElementById("minusbtn");
+// let count = document.getElementById("count");
+// let plusBtn = document.getElementById("plusbtn");
 
-let countNum = 0;
-count.innerHTML = countNum;
+// let countNum = 0;
+// count.innerHTML = countNum;
 
-minusBtn.addEventListener("click", () => {
-	countNum -= 1;
-	count.innerHTML = countNum;
+// minusBtn.addEventListener("click", () => {
+// 	countNum -= 1;
+// 	count.innerHTML = countNum;
+// });
+
+// plusBtn.addEventListener("click", () => {
+// 	countNum += 1;
+// 	count.innerHTML = countNum;
+// });
+
+
+$(document).ready(function() {
+    $('.minus').click(function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('.plus').click(function () {
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    });
 });
-
-plusBtn.addEventListener("click", () => {
-	countNum += 1;
-	count.innerHTML = countNum;
-});
-
-
