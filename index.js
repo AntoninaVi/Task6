c13ce57fb2c14381934eddea84491419fd5fbdcf
AntoninaVi <tonatonotonya@gmail.com>
2022-11-11 16:09:12 +0700

    forms

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
-// const fields = document.querySelector("form__input")
+const fields = document.querySelector(".form__input")
 
-// document.querySelector(".form__input").addEventListener("keyup", function () {
-//     this.value = this.value.replace(Value);
-// });
-// for (i = 0; i < fields.length; i++) {
-//     if (fields[i].value) {
-//         console.log('field is blank', fields[i])
+document.querySelector(".form__input").addEventListener("keyup", function () {
+    this.value = this.value.replace(Value);
+});
+for (i = 0; i < fields.length; i++) {
+    if (fields[i].value) {
+        console.log('field is blank', fields[i])
 
 
-// let minusBtn = document.getElementById("minusbtn");
-// let count = document.getElementById("count");
-// let plusBtn = document.getElementById("plusbtn");
-
-// let countNum = 0;
-// count.innerHTML = countNum;
-
-// minusBtn.addEventListener("click", () => {
-// 	countNum -= 1;
-// 	count.innerHTML = countNum;
-// });
-
-// plusBtn.addEventListener("click", () => {
-// 	countNum += 1;
-// 	count.innerHTML = countNum;
-// });
 
 
 
