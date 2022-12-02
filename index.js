ee96ae3bfd89a09b67f1ac573a298d574741ac30
AntoninaVi <tonatonotonya@gmail.com>
2022-12-02 17:46:31 +0700

    page 2

--- index.js
+++ index.js
@@ -1,143 +1,5 @@
-
-
-// CIRCLE LOADERS
-// $(function () {
-//     $('.chart').easyPieChart({
-//         size: 44,
-//         barColor: "#0880AE",
-//         scaleLength: 0,
-//         lineWidth: 3,
-//         trackColor: "#DBE2EA",
-//         lineCap: "circle",
-//         animate: 18000,
-//     });
-// });
-// $(function () {
-//     $('.chart2').easyPieChart({
-//         size: 44,
-//         barColor: "#F2AC57",
-//         scaleLength: 0,
-//         lineWidth: 3,
-//         trackColor: "#DBE2EA",
-//         lineCap: "circle",
-//         animate: 14000,
-//     });
-// });
-// $(function () {
-//     $('.chart3').easyPieChart({
-//         size: 44,
-//         barColor: "#14A38B",
-//         scaleLength: 0,
-//         lineWidth: 3,
-//         trackColor: "#DBE2EA",
-//         lineCap: "circle",
-//         animate: 18000,
-//     });
-// });
-// $(function () {
-//     $('.chart4').easyPieChart({
-//         size: 44,
-//         barColor: "#FF7171",
-//         scaleLength: 0,
-//         lineWidth: 3,
-//         trackColor: "#DBE2EA",
-//         lineCap: "circle",
-//         animate: 17000,
-//     });
-// });
-
-
-// COUNTERS
-
-let num1 = 0
-let sumEL = document.getElementById("sum")
-
-function minus1() {
-    let result = num1 - 1
-    sumEL.textContent = result
-    num1 = num1 - 1
-}
-console.log(num1)
-
-function add1() {
-    let result = num1 + 1
-    sumEL.textContent = + result
-    num1 = num1 + 1
-}
-console.log(num1)
-
-
-
-let num2 = 0
-let sumEL2 = document.getElementById("sum2")
-
-function minus2() {
-    let result = num2 - 1
-    sumEL2.textContent = result
-    num2 = num2 - 1
-}
-console.log(num2)
-
-function add2() {
-    let result = num2 + 1
-    sumEL2.textContent = + result
-    num2 = num2 + 1
-}
-console.log(num2)
-
-
-
-
-
-
-// RANGE SLIDERS
-
-const rangeInputs = document.querySelectorAll('input[type="range"]')
-
-function handleInputChange(e) {
-    let target = e.target
-    if (e.target.type !== 'range') {
-        target = document.getElementById('range')
-    }
-    const min = target.min
-    const max = target.max
-    const val = target.value
-
-    target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'
-}
-
-rangeInputs.forEach(input => {
-    input.addEventListener('input', handleInputChange)
-})
-
-
-
-// STAR RATING
-document.querySelector('jsuites-rating').addEventListener('onchange', function (e) {
-    document.getElementById('console').innerHTML = + this.value;
-});
-
-// BURGER MENU page 4
-// function myFunction() {
-//     var x = document.getElementById("myLinks");
-//     if (x.style.display === "inline-block") {
-//         x.style.display = "none";
-//     } else {
-//         x.style.display = "inline-block";
-//     }
-// }
-
-// function myFunction2() {
-//     var x = document.getElementById("myLinks2");
-//     if (x.style.display === "inline-block") {
-//         x.style.display = "none";
-//     } else {
-//         x.style.display = "inline-block";
-//     }
-// }
-
-
-// dropdown 2
+// PAGE 2
+//DROPDOWN BUTTONS
 function show_list() {
     var courses = document.getElementById("dropdown_id");
 
@@ -169,3 +31,119 @@ window.onclick = function (event) {
             .style.display = "none";
     }
 }   
+// COUNTERS
+let num1 = 0
+let sumEL = document.getElementById("sum")
+
+function minus1() {
+    let result = num1 - 1
+    sumEL.textContent = result
+    num1 = num1 - 1
+}
+console.log(num1)
+
+function add1() {
+    let result = num1 + 1
+    sumEL.textContent = + result
+    num1 = num1 + 1
+}
+console.log(num1)
+
+
+let num2 = 0
+let sumEL2 = document.getElementById("sum2")
+
+function minus2() {
+    let result = num2 - 1
+    sumEL2.textContent = result
+    num2 = num2 - 1
+}
+console.log(num2)
+
+function add2() {
+    let result = num2 + 1
+    sumEL2.textContent = + result
+    num2 = num2 + 1
+}
+console.log(num2)
+
+
+// RANGE SLIDERS
+
+const rangeInputs = document.querySelectorAll('input[type="range"]')
+
+function handleInputChange(e) {
+    let target = e.target
+    if (e.target.type !== 'range') {
+        target = document.getElementById('range')
+    }
+    const min = target.min
+    const max = target.max
+    const val = target.value
+
+    target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'
+}
+
+rangeInputs.forEach(input => {
+    input.addEventListener('input', handleInputChange)
+})
+
+
+// CIRCLE LOADERS
+var bar = new ProgressBar.Circle(containerProgress, {
+    strokeWidth: 10,
+    easing: 'easeInOut',
+    duration: 20000,
+    color: '#0880AE',
+    trailColor: '#D8D8D8',
+    trailWidth: 9,
+    svgStyle: null
+});
+
+bar.animate(1.0);
+
+var bar = new ProgressBar.Circle(containerProgress2, {
+    strokeWidth: 10,
+    easing: 'easeInOut',
+    duration: 21000,
+    color: '#F2AC57',
+    trailColor: '#D8D8D8',
+    trailWidth: 9,
+    svgStyle: null
+});
+
+bar.animate(1.0);
+
+
+var bar = new ProgressBar.Circle(containerProgress3, {
+    strokeWidth: 10,
+    easing: 'easeInOut',
+    duration: 22000,
+    color: '#14A38B',
+    trailColor: '#D8D8D8',
+    trailWidth: 9,
+    svgStyle: null
+});
+
+bar.animate(1.0);
+
+var bar = new ProgressBar.Circle(containerProgress4, {
+    strokeWidth: 10,
+    easing: 'easeInOut',
+    duration: 25000,
+    color: '#FF7171',
+    trailColor: '#D8D8D8',
+    trailWidth: 9,
+    svgStyle: null
+});
+
+bar.animate(1.0);
+
+
+// PAGE 4
+// STAR RATING
+document.querySelector('jsuites-rating').addEventListener('onchange', function (e) {
+    document.getElementById('console').innerHTML = + this.value;
+});
+
+
